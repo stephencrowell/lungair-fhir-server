3793ba1dc39fd6a49a5749b3901ed77cfb7df7b6
Stephen Crowell <stephen.crowell@kitware.com>
2022-10-14 13:14:26 -0400

    Update args to take json file for data source

--- data_sources/__init__.py
+++
@@ -1,5 +0,0 @@
-import json
-with open('args.json') as json_file:
-  json_dict = json.load(json_file)
-  for data_source in json_dict:
-  	exec('from .{0} import {1}'.format(data_source, json_dict[data_source]['import_name']))
